84118c2323f187a256725bb22efcc82d48cdd05d
David Meneses <David.Meneses@endava.com>
2022-08-23 11:35:48 -0500

    added dockerfiles and fixed the app

--- movie-analyst-api/server.js
+++ movie-analyst-api/server.js
@@ -22,7 +22,7 @@ app.get('/movies', async function (req, res) {
     res.json(rows)
   } catch (err) {
     console.error('API Error:', err)
-    res.staus(500).send({'msg': 'Internal server error'})
+    res.status(500).send({'msg': 'Internal server error'})
   }
 })
 
@@ -32,7 +32,7 @@ app.get('/reviewers', async function (req, res) {
     res.json(rows)
   } catch (err) {
     console.error('API Error:', err)
-    res.staus(500).send({'msg': 'Internal server error'})
+    res.status(500).send({'msg': 'Internal server error'})
   }
 })
 
@@ -42,7 +42,7 @@ app.get('/publications', async function (req, res) {
     res.json(rows)
   } catch (err) {
     console.error('API Error:', err)
-    res.staus(500).send({'msg': 'Internal server error'})
+    res.status(500).send({'msg': 'Internal server error'})
   }
 })
 
@@ -56,7 +56,7 @@ app.get('/pending', async function (req, res) {
     res.json(rows)
   } catch (err) {
     console.error('API Error:', err)
-    res.staus(500).send({'msg': 'Internal server error'})
+    res.status(500).send({'msg': 'Internal server error'})
   }
 })
 
