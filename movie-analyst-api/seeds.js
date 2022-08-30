528ef7e3ac5c667a64d8ce7d1d1914223303e0b4
David Meneses <David.Meneses@endava.com>
2022-08-30 08:49:48 -0500

    same as before

--- movie-analyst-api/seeds.js
+++ movie-analyst-api/seeds.js
@@ -5,10 +5,10 @@ async function main () {
   try {
     const pool = mysql.createPool({
       connectionLimit: 10,
-      host: process.env.DB_HOST || 'localhost',
-      user: process.env.DB_USER || 'applicationuser',
-      password: process.env.DB_PASS || 'applicationuser',
-      database: process.env.DB_NAME || 'movie_db'
+      host: process.env.DB_HOST || '',
+      user: process.env.DB_USER || '',
+      password: process.env.DB_PASS || '',
+      database: process.env.DB_NAME || ''
     })
     pool.query = util.promisify(pool.query)
 
